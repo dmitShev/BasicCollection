fa80a33915c4bcdb3771e53cbb562d4f8e9de4c2
student <student@C429U25>
2021-09-24 10:08:34 +0300

    Добавьте файлы проекта.

---
+++ BasicCollection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+
+
+namespace BasicCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList(); myList.Add("First"); myList.Add("Second");
+            myList.Add("Third");
+            myList.Add("Fourth");
+            foreach (string item in myList)
+            {
+                Console.WriteLine("Unsorted: {0}", item);
+            }
+
+            myList.Sort();
+            foreach (string item in myList)
+            {
+                Console.WriteLine("   Sorted: {0}", item);
+
+            }
+        }
+    }
+}
